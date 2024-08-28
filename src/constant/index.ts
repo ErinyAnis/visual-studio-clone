0c94fbc2bd2db4534b7b642d503abae39ae328c4
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2024-08-28 20:22:22 +0300

    updated icons paths

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,13 +1,13 @@
 export const extensionIconPaths: Record<string, string> = {
     // files
-    tsx: "/icons/react_ts",
-    js: "/icons/javascript",
-    jsx: "/icons/react",
-    html: "/icons/html",
+    tsx: "icons/react_ts",
+    js: "icons/javascript",
+    jsx: "icons/react",
+    html: "icons/html",
   
     //folders
-    node_modules: "/icons/folder-node",
-    public: "/icons/folder-public",
-    src: "/icons/folder-src",
-    components: "/icons/folder-components",
+    node_modules: "icons/folder-node",
+    public: "icons/folder-public",
+    src: "icons/folder-src",
+    components: "icons/folder-components",
   };
